a70e512d1d4d1dc802f1e3fd2052c8f4e615b6f3
resteve <resteve@zikzakmedia.com>
2015-12-01 01:22:42 +0100

    Tests

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part translate module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the translate module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TranslateTestCase(unittest.TestCase):
+class TranslateTestCase(ModuleTestCase):
     'Test Translate module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('translate')
-
-    def test0005views(self):
-        'Test views'
-        test_view('translate')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'translate'
 
 
 def suite():
